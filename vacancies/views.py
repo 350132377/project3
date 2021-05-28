d7b343053c99a28261e549e25b045df03df912d5
Natalia_K <natalia_natalia.1@mail.ru>
2021-05-28 22:45:11 +1000

    initial commit

--- vacancies/views.py
+++ vacancies/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
-# Create your views here.
+from baza_vakansiy_it.vacancies.data import jobs, companies, specialties
+
+
+def main_view(request):
+    return render(request, 'vacancies/index.html')
+
+def vacancy_view(request):
+    return render(request, 'vacancies/vacancy.html')
+
+def vacancies_view(request):
+    return render(request, 'vacancies/vacancies.html')
+
+def vacancies_frontend_view(request):
+    return render(request, 'vacancies/vacancy.html')
+
+def company_view(request):
+    return render(request, 'vacancies/company.html')

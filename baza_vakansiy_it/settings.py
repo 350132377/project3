2db0956d3acfd8746f62f158cab480dfdcbe614c
Natalia_K <natalia_natalia.1@mail.ru>
2021-05-29 01:41:58 +1000

    initial commit

--- baza_vakansiy_it/settings.py
+++ baza_vakansiy_it/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'baza_vakansiy_it.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [

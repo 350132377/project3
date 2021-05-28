75ab9c35064a4b2b968f70c00dc6388082ec9a56
Natalia_K <natalia_natalia.1@mail.ru>
2021-05-29 00:27:19 +1000

    initial commit

--- baza_vakansiy_it/settings.py
+++ baza_vakansiy_it/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ddn-!nxna+02l*xyn5ysfy5o7@gkikma$^u6*+isx#c*gk1h3_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition

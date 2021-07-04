045d70658c9b905196e0f4b887e824b7d4e0b1ec
Natalia_K <natalia_natalia.1@mail.ru>
2021-07-04 21:30:25 +1000

    initial commit

--- baza_vakansiy_it/urls.py
+++ baza_vakansiy_it/urls.py
@@ -19,7 +19,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from vacancies.views.main import main_view, company_view, vacancies_by_specialty
 from vacancies.views.company import MyCompanyView, MyCompanyCreateView
-from vacancies.views.vacancies import vacancies_view, vacancy_send, my_company_vacancies, my_company_vacancies_create, my_company_vacancy_id, ApplicationSendView, MyVacancyView
+from vacancies.views.vacancies import vacancies_view, vacancy_send, MyVacanciesListView, my_company_vacancies_create, my_company_vacancy_id, ApplicationSendView, MyVacancyView
 from django.urls import include
 
 
@@ -32,10 +32,10 @@ urlpatterns = [
     path('vacancies/<pk>/', ApplicationSendView.as_view(), name='application_vacancy'),
 
     # вакансии
-    # Отправка заявки, отклик отправлен
+    # work
     path('vacancies/<vacancy_id>/send/', vacancy_send, name='vacancy_send'),
     # список вакансий
-    path('mycompany/vacancies/', my_company_vacancies, name='myvacancy'),
+    path('mycompany/vacancies/', MyVacanciesListView.as_view(), name='myvacancy'),
     # пустая форма вакансия
     path('mycompany/vacancies/create/', my_company_vacancies_create, name='myvacancy_create'),
     # заполненная форма вакансия
--- vacancies/views/vacancies.py
+++ vacancies/views/vacancies.py
@@ -4,15 +4,8 @@ from django.shortcuts import get_object_or_404
 from django.views import View
 from vacancies.forms import ApplicationSendForm, MyVacanciesForm
 
-def vacancy_send(request, vacancy_id):
-    vacancy = get_object_or_404(Vacancy, pk=vacancy_id)
-    return render(request, 'vacancies/sent.html', context={
-        'vacancy': vacancy,
-    })
 
-# список
-def my_company_vacancies(request):
-    return render(request, 'vacancies/vacancy-list.html')
+
 # work пустая форма
 def my_company_vacancies_create(request):
     return render(request, 'vacancies/vacancy-edit.html')
@@ -27,7 +20,14 @@ def vacancies_view(request):
     return render(request, 'vacancies/vacancies.html', context={
         'specialities': Specialty.objects.all(),
     })
-
+# work
+def vacancy_send(request, vacancy_id):
+    vacancy = get_object_or_404(Vacancy, pk=vacancy_id)
+    return render(request, 'vacancies/sent.html', context={
+        'vacancy': vacancy,
+    })
+# отклик на вакансию
+# work
 class ApplicationSendView(View):
     def get(self, request, pk):
         vacancy = get_object_or_404(Vacancy, pk=pk)
@@ -35,19 +35,29 @@ class ApplicationSendView(View):
             'form': ApplicationSendForm,
             'vacancy': vacancy,
         })
-# не работает отправка отклика
+
     def post(self, request, pk):
         vacancy = get_object_or_404(Vacancy, pk=pk)
         form = ApplicationSendForm(request.POST)
         if form.is_valid():
             print(form.cleaned_data)
-            return redirect('vacancy_send', kwargs={'vacancy_id': pk})
+            return redirect('vacancy_send', vacancy_id=pk)
         return render(request, 'vacancies/vacancy.html', context={
             'form': form,
             'vacancy': vacancy,
         })
 
 # список вакансии пользователя
+# work
+class MyVacanciesListView(View):
+    def get(self, request):
+        try:
+            company = Company.objects.get(owner_id=request.user.id)
+        except Company.DoesNotExist:
+            return redirect('letsstart_mycompany')
+        vacancies = Vacancy.objects.filter(company_id=company.id)
+        return render(request, 'vacancies/vacancy-list.html', context={'vacancies': vacancies})
+
 class MyVacancyView(View):
     def get(self, request):
         return render(request, 'vacancies/vacancy-edit.html', context={

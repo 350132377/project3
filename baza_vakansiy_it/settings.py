b369050567b7d31cd29afb5a28838c417cde65d4
Natalia_K <natalia_natalia.1@mail.ru>
2021-07-15 22:26:09 +1000

    initial commit

--- baza_vakansiy_it/settings.py
+++ baza_vakansiy_it/settings.py
@@ -23,10 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ddn-!nxna+02l*xyn5ysfy5o7@gkikma$^u6*+isx#c*gk1h3_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
-
+ALLOWED_HOSTS = ['*', '127.0.0.1']
 
 # Application definition
 
@@ -120,6 +119,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+STATIC_ROOT = 'static'
 STATIC_URL = '/static/'
 
 # Default primary key field type
